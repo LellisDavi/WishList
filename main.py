9aeaaed9cc05c40564dc069c94b728cfc7f8baa5
Davi <davilellis1@gmail.com>
2026-08-31 18:08:24 -0300

    cria classe repositorio com metodos de criar tabelas, inserir categoria e fechar conexao

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from models.categoria import Categoria
 from models.item import Item
+from repositorio import Repositorio
 
 def main():
     saude=Categoria('saude','necessidade')
     item1=Item('Escova de Dente', 20, categoria=saude,status='pendente') 
     print(item1.obter_tipo())
+
+    
 if __name__ == '__main__':
     main()
--- repositorio/data/repositorio.py
+++ repositorio/data/repositorio.py
@@ -1,20 +1,30 @@
 import sqlite3
 
-banco = sqlite3.connect('database/wishlist.db')
-cursor = banco.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS categoria (" \
-"id integer PRIMARY KEY AUTOINCREMENT," \
-" nome text," \
-" tipo text)")
+class Repositorio():
+    def __init__(self):
+        self.banco = sqlite3.connect('database/wishlist.db')
+        self.cursor = self.banco.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS item (" \
-"id integer PRIMARY KEY AUTOINCREMENT," \
-" nome text," \
-" preco double," \
-" categoria_id integer," \
-" status text," \
-" FOREIGN KEY (categoria_id) REFERENCES categoria(id))")
+    def criar_tabelas(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS categoria (" \
+        "id integer PRIMARY KEY AUTOINCREMENT," \
+        " nome text," \
+        " tipo text)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS item (" \
+        "id integer PRIMARY KEY AUTOINCREMENT," \
+        " nome text," \
+        " preco double," \
+        " categoria_id integer," \
+        " status text," \
+        " FOREIGN KEY (categoria_id) REFERENCES categoria(id))")
+        self.banco.commit()
 
-banco.commit()
-banco.close()
+    def fechar_conexao(self):
+        self.banco.close()    
+
+
+    def inserir_categoria(self,categoria):
+        self.cursor.execute("INSERT INTO categoria(nome, tipo) VALUES (?,?)", (categoria.nome, categoria.tipo)) 
+         
+j
