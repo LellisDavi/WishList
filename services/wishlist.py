446e8fae23672a7d3a6e06526e5d08d49acfb88a
Davi <davilellis1@gmail.com>
2026-09-03 19:56:44 -0300

    Inicio da criação do metodo adicionar categoria (ele verifica se ja existe a categoria no banco)

--- services/wishlist.py
+++ services/wishlist.py
@@ -1,4 +1,14 @@
 from repositorio.repositorio import Repositorio
+from models.categoria import Categoria
+from models.item import Item
+
+
 class Wishlist():
     def __init__(self):
         self.repositorio = Repositorio()
+
+
+    def adicionar_categoria(self,nome,tipo):
+        categorias_existentes = self.repositorio.buscar_categoria()
+        for categoria in categorias_existentes():
+            if nome == categorias_existentes.nome:
