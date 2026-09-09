e0e7cc4d68857d43ce25d9f5ff08627fe6ade3c2
Davi <davilellis1@gmail.com>
2026-09-09 16:07:03 -0300

    adiciona metodo adicionar_categoria com verificacao de duplicidade

--- services/wishlist.py
+++ services/wishlist.py
@@ -10,5 +10,11 @@ class Wishlist():
 
     def adicionar_categoria(self,nome,tipo):
         categorias_existentes = self.repositorio.buscar_categoria()
-        for categoria in categorias_existentes():
-            if nome == categorias_existentes.nome:
+        for l in categorias_existentes:
+            if nome.lower() == l.nome.lower():
+                return f"Essa categoria já existe."
+        nova_categoria = Categoria(nome, tipo)
+        self.repositorio.inserir_categoria(nova_categoria)    
+            
+            
+
