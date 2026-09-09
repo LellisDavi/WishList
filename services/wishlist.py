0640a70a2c5e3e7d53cf00c6cb2f1dcba49f14ea
Davi <davilellis1@gmail.com>
2026-09-09 16:22:45 -0300

    adiciona o metodo adicionar_item na wishlist

--- services/wishlist.py
+++ services/wishlist.py
@@ -14,7 +14,21 @@ class Wishlist():
             if nome.lower() == l.nome.lower():
                 return f"Essa categoria já existe."
         nova_categoria = Categoria(nome, tipo)
-        self.repositorio.inserir_categoria(nova_categoria)    
+        self.repositorio.inserir_categoria(nova_categoria)
+
+
+    def adicionar_item(self, nome_item, preco, nome_categoria, status):
+        categoria_encontrada = None
+        categorias_existentes = self.repositorio.buscar_categoria()
+        for l in categorias_existentes:
+            if nome_categoria.lower() == l.nome.lower():
+                categoria_encontrada = l
+            else:
+                return "Categoria não encontrada, cadastre-a primeiro!"    
+        if categoria_encontrada is not None:
+            novo_item = Item(nome_item, preco, categoria_encontrada, status)
+            self.repositorio.inserir_item(novo_item, categoria_encontrada.id)
+
             
             
 
