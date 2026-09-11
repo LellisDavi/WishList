eea34510097238b57a477c5373da6634d20f2747
Davi <davilellis1@gmail.com>
2026-09-11 10:04:24 -0300

    v1 funcional: menu completo testado e funcionando

--- main.py
+++ main.py
@@ -6,6 +6,36 @@ def main():
 
     while True:
         print('--- MENU WISHLIST --- ')
-        print('[1] ADCIONAR CATEGORIA \n [2] ADICIONAR ITEM \n [3] LISTAR ITENS \n [4] MARCAR ITEM \n [5] LISTAR ADQUIRIDOS \n [6] SAIR')
+        print(''
+        ' [1] ADICIONAR CATEGORIA ' \
+        '\n [2] LISTAR CATEGORIA ' \
+        '\n [3] ADICIONAR ITEM ' \
+        '\n [4] LISTAR ITENS ' \
+        '\n [5] MARCAR ITEM ' \
+        '\n [6] LISTAR ADQUIRIDOS ' \
+        '\n [7] SAIR')
+
+        escolha = int(input('Digite sua opção: '))
+        if escolha == 1:
+            nome = input('Nome da categoria: ')
+            tipo = input('Tipo da categoria: ')
+            wishlist.adicionar_categoria(nome, tipo)
+        elif escolha == 2:
+             wishlist.listar_categorias()  
+        elif escolha == 3:
+            nome = input('Nome do item: ')
+            preco = float(input('Preço do item: '))
+            nome_categoria = input('Nome da categoria: ')
+            wishlist.adicionar_item(nome, preco, nome_categoria, status='pendente') 
+        elif escolha == 4:
+            wishlist.listar_itens()
+        elif escolha == 5:
+            id_item = int(input('Informe o id do item: '))   
+            wishlist.marcar_como_adquirido(id=id_item) 
+        elif escolha == 6:
+            wishlist.listar_adquiridos()     
+        elif escolha == 7:
+            break    
+
 if __name__ == '__main__':
     main()
